2b7f964aca6cf6b721f025a821ff1822074cee3d
Alexey Filippov <afilippov@acm.org>
2020-04-19 23:03:20 +0100

    AoC 2015 Day 2 task 2

--- AoC2015/Program.fs
+++ AoC2015/Program.fs
@@ -14,55 +14,69 @@ let multi_line_input day =
 let single_line_input day =
     Seq.head (multi_line_input day)
 
-let day01_1 =
-    let floor s =
-        let mutable n = 0
+module Day1 =
+    let part1 =
+        let floor s =
+            let mutable n = 0
 
-        let consume c =
-            match c with
-            | '(' -> n <- n + 1
-            | ')' -> n <- n - 1
-        String.iter consume s
-        n
-    floor (single_line_input 1)
+            let consume c =
+                match c with
+                | '(' -> n <- n + 1
+                | ')' -> n <- n - 1
+            String.iter consume s
+            n
+        floor (single_line_input 1)
 
-let day01_2 =
-    let time_to_basement (s: string): int =
-        let mutable i = 0
-        let mutable n = 0
+    let part2 =
+        let time_to_basement (s: string): int =
+            let mutable i = 0
+            let mutable n = 0
 
-        while n <> -1 do
-            match s.[i] with
-            | '(' -> n <- n + 1
-            | ')' -> n <- n - 1
-            i <- i + 1
-        i
-    time_to_basement (single_line_input 1)
+            while n <> -1 do
+                match s.[i] with
+                | '(' -> n <- n + 1
+                | ')' -> n <- n - 1
+                i <- i + 1
+            i
+        time_to_basement (single_line_input 1)
 
-let day02_1 =
-    let area w l h =
-        let sides =
-            [ l * w
-              w * h
-              h * l ]
-
-        let minSide = List.min sides
-        2 * (List.sum sides) + minSide
-
+module Day2 =
     let dimensions (s: string) =
         let parts = s.Split [| 'x' |]
         let dims = Seq.map int parts
         (Seq.item 0 dims, Seq.item 1 dims, Seq.item 2 dims)
 
-    let task = multi_line_input 2
-    let areas = Seq.map (dimensions >> fun (w, l, h) -> area w l h) task
-    Seq.sum areas
+    let task = multi_line_input 2 |> Seq.map dimensions
+
+    let part1 =
+        let area (w, l, h) =
+            let sides =
+                [ l * w
+                  w * h
+                  h * l ]
+
+            let minSide = List.min sides
+            2 * (List.sum sides) + minSide
+
+        Seq.map area task |> Seq.sum
+
+    let part2 =
+        let ribbon (w, l, h) =
+            let sides =
+                [ l + w
+                  w + h
+                  h + l ]
+
+            let minSide = List.min sides
+            2 * minSide + w * l * h
+
+        Seq.map ribbon task |> Seq.sum
 
 
 [<EntryPoint>]
 let main argv =
-    printfn "Day 1 part 1 answer: %i" day01_1
-    printfn "Day 1 part 2 answer: %i" day01_2
-    printfn "Day 2 part 1 answer: %i" day02_1
-    //    printfn "Day 2 part 2 answer: %i" day02_2
+    printfn "Day 1 part 1 answer: %i" Day1.part1
+    printfn "Day 1 part 2 answer: %i" Day1.part2
+    printfn "Day 2 part 1 answer: %i" Day2.part1
+    printfn "Day 2 part 2 answer: %i" Day2.part2
     0
